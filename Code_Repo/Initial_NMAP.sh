b54e65e56c0f15d08879288a4a65fff289f3c293
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2024-09-10 17:46:44 +1000

    Create Initial_NMAP.sh

---
+++ Code_Repo/Initial_NMAP.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Argument Supplied Check
+display_usage() { 
+	echo -e "\nUsage: $0 <IP Address>\n" 
+	} 
+# if less than 3 arguments supplied, display usage 
+if [  $# -le 0 ] 
+then 
+	display_usage
+	exit 1
+fi 
+ 
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $@ == "--help") ||  $@ == "-h" ]] 
+then 
+	display_usage
+	exit 0
+fi 
+
+# Variable Assignment
+ip=$1
+scriptDir=$(pwd)
+
+mkdir $ip && cd $ip
+mkdir nmapScans && cd nmapScans
+
+echo -e "===== Folders Created =====\n"
+pwd
+echo -e "\n===== Begining Nmap Port Scan =====\n"
+
+#nmap scan to identify all open port
+sudo nmap -sUT -p- -Pn --min-rate 10000 -T4 $ip --open -o nmapScan.txt
+
+#formating TCP port numbers so we can perform service scans
+cat nmapScan.txt | grep "/tcp\|/udp" | cut -d "/" -f 1 > Ports.txt
+awk '{print $1}' Ports.txt | paste -s -d, - > PortList.txt
+
+#scan for services on the open ports
+echo -e "\n===== Begining Nmap Service Scan =====\n"
+sudo nmap -sUT --min-rate 10000 -p $(cat PortList.txt) -sVC -Pn $ip --open -o sVC_Port_Scan.txt
+
+# scan for UDP ports/services
+
+#open ports for report 
+cat sVC_Port_Scan.txt | grep "PORT\|open" > ports_for_report.txt
+
+#start web enum if HTTP ports are open
+echo -e "\n===== Begining Web Service Scan =====\n"
+$scriptDir/Web_Enum.sh
